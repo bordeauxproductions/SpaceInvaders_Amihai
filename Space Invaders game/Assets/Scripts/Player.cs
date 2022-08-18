2dd24a86719e0062eb71dd5c08fe4efb8edfdde6
Amihai Blekherov <amihaibl@post.bgu.ac.il>
2022-08-18 10:59:06 +0300

    Creating the laser shooting, including the collisions it has with the other game components.

--- Space Invaders game/Assets/Scripts/Player.cs
+++ Space Invaders game/Assets/Scripts/Player.cs
@@ -4,7 +4,9 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    public Projectile laserPrefab;
     public float speed = 5.0f;
+    public bool _laserActive;
 
     // Start is called before the first frame update
     void Start()
@@ -15,8 +17,9 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //Difference between 'GetKey' and 'GetKeyDown' is that 'GetKey' will keep on changing the position if the key is pressed down,
-        //while 'GetKeyDown' will change it only once per frame is the key is being pressed down.
+        //Player movement: Left & Right 
+        /*The difference between 'GetKey' and 'GetKeyDown' is that 'GetKey' will keep on changing the position if the key is pressed down,
+          while 'GetKeyDown' will change it only once per frame is the key is being pressed down. */
         if(Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
         {
             this.transform.position += Vector3.left * this.speed * Time.deltaTime;
@@ -26,5 +29,33 @@ public class Player : MonoBehaviour
         {
             this.transform.position += Vector3.right * this.speed * Time.deltaTime;
         }
+
+        //Shooting 'laser'
+        //Here we want to use 'GetKeyDown' since we want to shoot only once even if the key is pressed down (and not spray automatically).
+        if (Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0))
+        {
+            Shoot();
+        }
+    }
+
+    //Shooting the 'laser'
+    private void Shoot()
+    {
+        //We don't want multiple lasers in the scene, only one per move (until it is gone from the scene)
+        if (!_laserActive)
+        {
+            //Subscribe to the event
+            Projectile projectile = Instantiate(this.laserPrefab, this.transform.position, Quaternion.identity);
+            //Once the laserPrefab (Projectile object) was created, it will update automatically every frame - meaning the position 
+            //of this specific 'laser' particle will change every frame - going upwards in the game.
+
+            _laserActive = true; //Won't allow another laser to be shot until this one is destroyed
+            projectile.destroyed += LaserDestryoed; //Now we can shoot again.
+        }
+    }
+
+    private void LaserDestryoed()
+    {
+        _laserActive = false;
     }
 }
